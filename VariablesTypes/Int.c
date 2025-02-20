b26f302319b12e269b64013f026e0a98a6e3cce6
wyonyango <wycliffealphus7@gmail.com>
2025-02-20 12:49:27 +0300

    feat:add int

---
+++ VariablesTypes/Int.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(void) {
+    int age = 0;
+
+    age = 37.2;
+
+    printf("%u", age);
+}
+
+
+// Built in data types include int, char, short, long, float, double, long double.
