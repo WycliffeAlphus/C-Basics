47a3c17ff01fb7c562df86183acf75e0b942fd0f
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-23 09:57:24 +0300

    add switch

---
+++ conditionals/switch/switch.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(void){
+
+int a = 1;
+
+switch(a) {
+    case 0:
+    /*work*/
+    break;
+    case 1:
+    /*work*/
+    break;
+    case 3:
+    /*work*/
+    break;
+    default:
+    /*other cases*/
+    break;
+}
+
+// In c the break keyword is important, it does not exit
+// after returning true
+
+}
