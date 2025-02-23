9763802fb867e3155527304ee7cd67b318ff7f26
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-23 09:50:36 +0300

    add conditional

--- conditionals/cond.c
+++ conditionals/cond.c
@@ -9,3 +9,19 @@ int main(void){
         printf("%s\n", "no");
     }
 }
+
+// never use =  when checking conditional for it will always be true
+// Unless it is 0 ; 
+
+// for example
+
+/*
+int a = 0;
+
+if (a = 0){
+will never be invode because when 0 resolves to 
+bool it becomes false always
+All others including -1 resolves to true
+}
+
+*/
