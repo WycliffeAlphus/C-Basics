c4a02afcca52f93ecf33275caaa61678e72c39e8
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-23 10:31:11 +0300

    add break keyword

---
+++ loops/breakingout.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int main(){
+    for(int i = 0; i <= 10; i++){
+         printf("%u\n", i);
+        if (i == 4 )break;
+    }
+}
+
+// The break keyword helps in breaking out of a loop at a given point in time
