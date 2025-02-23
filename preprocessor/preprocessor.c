dbaf5e141c00bdef18cc240c38a6af25f20d9a7a
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-23 22:04:45 +0300

    add preprocessor

---
+++ preprocessor/preprocessor.c
@@ -0,0 +1,9 @@
+// The preprocessor parses the program and ensures
+// the compiler has all it needs
+// It llos at all the header files included using the 
+
+// #include directive and all constants defined using #define and substituted with
+// actual values
+
+// anything starting with # is taken care of by the preprocessor
+// It is considered a preprocessor directive
