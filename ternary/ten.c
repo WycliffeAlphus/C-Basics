5171cef7f330ac7aa753b547f47f8b03fa68db8b
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-23 09:29:36 +0300

    add ternary

---
+++ ternary/ten.c
@@ -0,0 +1,10 @@
+// <condition> ? <expression> : <expression>
+
+int main(void){
+    int a = 3;
+    int b =  2;
+    int c = 0;
+    a ? b : c ;
+
+    // if a is true, execute b else execute c
+}
