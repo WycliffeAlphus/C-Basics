190deb13d973501be1669d40eb08921b0804db08
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-23 20:23:04 +0300

    add typedef

---
+++ typedef/typedef.c
@@ -0,0 +1,19 @@
+// typedef allows one to define new types
+
+
+// syntax is:
+
+// typedef existingtype NEWTYPE
+
+// By convention, they are uppercase to easily recognize them
+
+
+#include <stdio.h>
+
+typedef int NUMBER;
+
+int main(void){
+NUMBER one = 1;
+}
+
+// they are inportant when paired with enumurated types and structures
