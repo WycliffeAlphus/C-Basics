d5c3e9be31ba73d74fe5dbc67b5455d843c2e56a
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-23 08:27:11 +0300

    feat:add wrong type assignment

---
+++ WrongTypes/wrong.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main(void) {
+    char j = 1000; 
+}
+
+//The example will complain because char type takes one byte(8 bit)
+// signed = -128 to 127
+// unsigned is = 0 to 255
