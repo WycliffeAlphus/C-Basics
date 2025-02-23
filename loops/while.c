c3160d539356168e939180f54b9b1d5a64bd69d5
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-23 10:17:43 +0300

    add while loop

---
+++ loops/while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void){
+int i = 0;
+while (i < 10) {
+    /*work*/
+    i++;
+}
+}
+
+/*
+It assumes that the variable is defined somewhere in the code
+and i need to be incremented inside the code block or else
+it will be an infinite loop
+*/
