11659ad7519a4a90577fed8f14e71438bd100217
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-24 05:47:59 +0300

    add ifdefined

--- ifdefined/if.c
+++ ifdefined/if.c
@@ -13,3 +13,20 @@ int main(void) {
     printf("Value is not defined\n");
 #endif        
 }
+
+
+// if defined also works
+
+// to check macros not defined use:
+// #ifndef or #if !defined
+
+/*
+
+#define DEBUG 0
+
+#if DEBUG
+   // code only sent to compiler 
+   // if DEBUG is not 0
+#endif
+
+*/
