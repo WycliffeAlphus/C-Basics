62179c9ca71c8770d1226debc048439928d55f8f
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-24 06:03:12 +0300

    feat:add predefined symbolic constant

---
+++ preSymb/pre.c
@@ -0,0 +1,11 @@
+// The preprocessor defines a  number of symbolic constants that one can use
+// They are identified by 2 underscores before and after the name
+
+/*
+
+_LINE_    >> current file name as a string
+_FILE_    >> current line number in the file
+_DATE_    >> compilation date
+_TIME_    >> compilation time
+
+*/
