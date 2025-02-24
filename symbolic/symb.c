b48be464ec52bca7c3c78ac0aef3a78c32c3592a
WycliffeAlphus <wycliffealphus7@gmail.com>
2025-02-24 05:27:12 +0300

    feat:add symbolic

---
+++ symbolic/symb.c
@@ -0,0 +1,7 @@
+#define VALUE 1
+#define PI 3.14
+#define NAME "Flavio"
+
+// the preprocessor will replace the names with values
+// one can give a name to a value without creating the variable at compile time
+
